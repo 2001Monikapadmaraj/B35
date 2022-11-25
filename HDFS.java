04717cff118e746e05e65401631d3f0fc18b46fa
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:57:53 +0530

    Add files via upload

---
+++ HDFS.java
@@ -0,0 +1,22 @@
+package org.tnsindia.Encapsulation.java;
+
+public class HDFS {
+	private double amount;
+	
+	public double getAount()
+	{
+		return amount;
+	}
+	 
+	public void steAmount(double amount)
+	{
+		this.amount=amount;
+		
+	}
+}
+	
+
+	
+	
+
+
