30a9b897f1a5b267859752e4c2bd45d297e2e226
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:45:19 +0530

    Add files via upload

---
+++ ConstructorExecutor.java
@@ -0,0 +1,12 @@
+package Day4.org.tnsindia.Constructor;
+
+public class ConstructorExecutor {
+
+	public static void main(String[] args) {
+        employee e=new employee();
+        employee e1=new employee();
+        e1.print();
+        
+	}
+
+}
