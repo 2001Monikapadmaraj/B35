cbffe1704ff0b23bb305fef597390c7f7b74db14
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 14:46:31 +0530

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,16 @@
+
+package DAY3.org.tnsindia;
+import java.util.Scanner;
+public class Day3 {
+
+     public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("enter the value of and b");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int result=a+b;
+		System.out.println("the result is:"+result);
+		s.close();
+	}
+
+}
