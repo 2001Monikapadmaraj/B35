46f10633247b04ee534a8d3aaa6927baafb42f7e
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:59:58 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,12 @@
+package org.tnsindia.Package.java;
+
+public class ATM {
+	public String card_no;
+	 public void display(String card_no)
+	 
+	 {
+		 System.out.println("print the ATM card number"+card_no);
+		 
+	 }
+
+}
