ced6bd7d48598dd689d54e397a8192a5f27b1586
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:58:56 +0530

    Add files via upload

---
+++ HDFSExecution.java
@@ -0,0 +1,13 @@
+package org.tnsindia.Encapsulation.java;
+
+public class HDFSExecution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HDFS h=new HDFS();
+		h.steAmount(557678);
+		System.out.println("the amount is:"+h.getAount());
+
+	}
+
+}
