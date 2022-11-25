bc1bec766e1f02c27276fbcfc5c59d166879883c
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:51:03 +0530

    Add files via upload

---
+++ Defaultconstructor.java
@@ -0,0 +1,11 @@
+package org.tnsindia.AccessSpecifier;
+
+public class Defaultconstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Default d=new Default(45);
+       d.display();
+	}
+
+}
