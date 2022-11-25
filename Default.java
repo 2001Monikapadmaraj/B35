41b6e0018c1d27ff3c1dd57560f8e3a9b7da5cb0
Monika Padmaraj <112746277+2001Monikapadmaraj@users.noreply.github.com>
2022-11-25 16:47:16 +0530

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,18 @@
+package org.tnsindia.AccessSpecifier;
+
+public class Default {
+	int speed;
+	Default(int speed){
+		super();
+		this.speed=speed;
+	}
+	 void display()
+	 {
+		 System.out.println("this speed is:"+speed);
+	 }	 
+
+	
+
+	}
+
+
